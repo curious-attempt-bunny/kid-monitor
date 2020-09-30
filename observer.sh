ed5fe8c078e8a47753d90bb227c3703f7976984a
Merlyn Albery-Speyer <merlyn@newrelic.com>
2020-09-29 23:16:43 -0700

    Fix binding.

--- observer.sh
+++ observer.sh
@@ -4,4 +4,4 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 echo "SCRIPTPATH is $SCRIPTPATH"
 cd $SCRIPTPATH
 source env/bin/activate
-FLASK_APP=observer.py python -m flask run
+FLASK_APP=observer.py python -m flask run --host=0.0.0.0
